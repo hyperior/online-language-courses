63b99406203dcefa41488b6b04ae16a308b28683
Ronald Graciel <ronaldgraciel@gmail.com>
2023-11-30 11:10:53 -0400

    add unit testing

---
+++ test/UtilsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AlphaRemote\test\PhpProject;
+
+use PHPUnit\Framework\TestCase;
+
+final class UtilLengthValidTest extends TestCase
+{
+
+    public function testArgumentValid(){
+
+
+
+    }
+
+}
